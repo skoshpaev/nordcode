a0f96ca570407181b33298e26f23867bd1de820c
skoshpaev <sts.ko@mail.ru>
2021-01-17 14:33:44 +0300

    first commit - docker environment and symphony framework have been prepared

---
+++ install.sh
@@ -0,0 +1,15 @@
+cp docker/docker-compose-test.yml.example docker-compose-test.yml
+cp docker/docker-compose.yml.example docker-compose.yml
+cp docker/var.env.example docker/var.env
+cp docker/var.env.example docker/var_test.env
+cp .env .env.local
+
+docker network create nordcode
+docker network create nordcode_test
+alias dd='docker-compose'
+alias console='docker-compose exec fpm bin/console'
+dd up -d
+
+dd exec fpm composer install
+
+sh test.sh
